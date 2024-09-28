a08e9fdf7e592eed44d0237f742016e2774deb8d
Adam Nash <adam@smr.llc>
2024-09-28 18:22:38 -0400

    get it working

---
+++ lambda/api-llama/index.ts
@@ -0,0 +1,49 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime';
+
+const client = new BedrockRuntimeClient();
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    try {
+        const body = JSON.parse(event.body || '{}');
+        const prompt = body.prompt || '';
+        const system = body.system || 'You are a helpful AI assistant';
+
+        if (!prompt) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'Input text is required' }),
+            };
+        }
+
+        let fullPrompt = `<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n${system}<|eot_id|>\n`
+        fullPrompt += `<|start_header_id|>user<|end_header_id|>\n\n${prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>`
+
+        const command = new InvokeModelCommand({
+            modelId: "meta.llama3-8b-instruct-v1:0",
+            contentType: 'application/json',
+            accept: 'application/json',
+            body: JSON.stringify({
+                prompt: fullPrompt,
+                max_gen_len: 1024
+            }),
+        });
+
+        const response = await client.send(command);
+        const modelResponse = JSON.parse(response.body.transformToString() || '{}');
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Success',
+                result: modelResponse,
+            }),
+        };
+    } catch (error) {
+        console.error('Error invoking Bedrock model:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Failed to invoke the model', error }),
+        };
+    }
+};
--- lib/jam-llama-stack.ts
+++ lib/jam-llama-stack.ts
@@ -1,16 +1,80 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpJwtAuthorizer } from "aws-cdk-lib/aws-apigatewayv2-authorizers";
+import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
+import * as cm from "aws-cdk-lib/aws-certificatemanager";
+import * as njslambda from "aws-cdk-lib/aws-lambda-nodejs";
+import { join } from 'path';
 
 export class JamLlamaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const baseDomain = "jamlaunch.com"
+    const subdomain = "llama"
+    const fullDomain = `${subdomain}.${baseDomain}`
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'JamLlamaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const zone = route53.HostedZone.fromLookup(this, "HostZone", {
+      domainName: baseDomain,
+    });
+
+    const cert = new cm.Certificate(this, "Cert", {
+      domainName: fullDomain,
+      validation: cm.CertificateValidation.fromDns(zone),
+    });
+    const domainName = new apigateway.DomainName(this, "ApiDomain", {
+      domainName: fullDomain,
+      certificate: cert
+    })
+
+    const serverAuth = new HttpJwtAuthorizer(
+      "ServerJwtAuth",
+      `https://oid.jamlaunch.com`,
+      {
+        jwtAudience: ["jamlaunch-server-v2"],
+      }
+    )
+    const api = new apigateway.HttpApi(this, 'Api', {
+      defaultDomainMapping: {
+        domainName: domainName
+      },
+      defaultAuthorizer: serverAuth,
+    });
+
+    new route53.ARecord(this, "ApiDns", {
+      zone: zone,
+      recordName: subdomain,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.ApiGatewayv2DomainProperties(
+          domainName.regionalDomainName,
+          domainName.regionalHostedZoneId
+        )
+      ),
+    });
+
+    const llamaFunc = new njslambda.NodejsFunction(this, "LlamaFunc", {
+      entry: join(__dirname, "../lambda/api-llama/index.ts"),
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+      },
+    });
+    llamaFunc.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['bedrock:InvokeModel'],
+      resources: ['*'],
+    }));
+    const llamaIntegration = new integrations.HttpLambdaIntegration("LlamaIntegration", llamaFunc)
+    api.addRoutes({
+      path: '/hello',
+      methods: [apigateway.HttpMethod.POST],
+      integration: llamaIntegration
+    });
+
   }
 }
